a9f45f183b5fe08997d74753b0a57e7977c0576e
wing wai <leongwingwai991218@gmail.com>
2026-05-24 21:01:35 +0800

    Update project.py

--- project.py
+++ project.py
@@ -12,16 +12,19 @@ import matplotlib.pyplot as plt
 # --- CONFIGURATION ---
 st.set_page_config(layout="wide", page_title="YouTube Trailer Sentiment Intelligence Dashboard")
 
-import gdown
-import os
-
-if not os.path.exists("model.pkl"):
-    gdown.download("https://365umedumy-my.sharepoint.com/:u:/g/personal/24074889_siswa365_um_edu_my/IQDAIlLiZQbRQL-3_-k-w4qbAeQLmDBhZOObUybFHYYIg74?e=FP8mXw", "model.pkl", quiet=False)
-# Then load your model as usual
 
 @st.cache_resource
 def load_assets():
-    model = joblib.load('model.pkl')
+    model_path = 'model.pkl'
+    # Check if model exists; if not, download it
+    if not os.path.exists(model_path):
+        # Use a public Google Drive URL
+        url = 'https://365umedumy-my.sharepoint.com/:u:/g/personal/24074889_siswa365_um_edu_my/IQDAIlLiZQbRQL-3_-k-w4qbAeQLmDBhZOObUybFHYYIg74?e=fRsbi2'
+        gdown.download(url, model_path, quiet=False)
+    
+    # Load model
+    model = joblib.load(model_path)
+    # Load tokenizer
     tokenizer = AutoTokenizer.from_pretrained('distilbert-base-uncased')
     model.eval()
     return model, tokenizer
